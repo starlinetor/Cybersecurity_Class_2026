afdaf52d79558ef4b90c8fc88abb67a4421d14d0
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2026-05-06 22:07:36 +0200

    created classes for output dictionary, created evalute request function, created evaluate request function, created update summary function, created test policy function, created frame for test functions

--- topic-a-authorization/topic-a-authorization/src/engine.py
+++ topic-a-authorization/topic-a-authorization/src/engine.py
@@ -5,13 +5,29 @@ class Summary(TypedDict):
     permitted_requests: int
     denied_requests: int
 
-class Evaluated_Requests(TypedDict):
-    summary: Summary
-    results: list[dict[str,Any]]
+class RequestResult(TypedDict):
+    request_id : str
+    decision : str
+    matched_policy : str | None
+    matched_conditions : list[str]
 
-def evaluate_requests(subjects_id : dict[str, dict], resources_id : dict[str, dict], policies_list : list[dict], requests_stack : list[dict]) -> Evaluated_Requests:
-    
-    evaluated_requests : Evaluated_Requests = {
+class EvaluatedRequests(TypedDict):
+    summary: Summary
+    results: list[RequestResult]
+
+def evaluate_requests(subjects_id : dict[str, dict], resources_id : dict[str, dict], policies_list : list[dict], requests_stack : list[dict]) -> EvaluatedRequests:
+    """Policy evaluation engine that evaluates a list of requests against the provided subjects, resources and policies.
+
+    Args:
+        subjects_id (dict[str, dict]): The dictionary by id of the subjects
+        resources_id (dict[str, dict]): The dictionary by id of the resources
+        policies_list (list[dict]): The list of policies
+        requests_stack (list[dict]): The list of requests to evaluate
+
+    Returns:
+        EvaluatedRequests: The evaluated requests with summary and results
+    """
+    evaluated_requests : EvaluatedRequests = {
         "summary": {
             "total_requests": 0,
             "permitted_requests": 0,
@@ -21,6 +37,113 @@ def evaluate_requests(subjects_id : dict[str, dict], resources_id : dict[str, di
     }
 
     for request in requests_stack:
-        continue
+        request_result = evaluate_request(subjects_id, resources_id, policies_list, request)
+        update_summary(evaluated_requests["summary"], request_result)
+        evaluated_requests["results"].append(request_result)
     
     return evaluated_requests
+
+def evaluate_request(subjects_id : dict[str, dict], resources_id : dict[str, dict], policies_list : list[dict], request : dict) -> RequestResult:
+    """Evaluates a single request against the provided subjects, resources and policies and returns the evaluation result.
+
+    Args:
+        subjects_id (dict[str, dict]): The dictionary by id of the subjects
+        resources_id (dict[str, dict]): The dictionary by id of the resources
+        policies_list (list[dict]): The list of policies
+        request (dict): The request to evaluate
+
+    Returns:
+        RequestResult: Result of the request evaluation with evaluation details
+    """
+    evaluated_request : RequestResult = {
+        "request_id" : request["request_id"],
+        "decision" : "Deny",
+        "matched_policy" : None,
+        "matched_conditions" : []
+    }
+    subject_id = request["subject_id"]
+    resource_id = request["resource_id"]
+    
+    try:
+        subject = subjects_id[subject_id]
+        resource = resources_id[resource_id]
+    except KeyError: #if retrieval fails return Deny, no need to check policies
+        return evaluated_request
+    
+    for policy in policies_list: 
+        result, matched_conditions = test_policy(policy, subject_id, subject, resource, resource_id, request)
+        if result:
+            evaluated_request["decision"] = "Permit"
+            evaluated_request["matched_policy"] = policy["id"]
+            evaluated_request["matched_conditions"] = matched_conditions
+            return evaluated_request #policy found that matches the request, return Permit and stop searching for other policies
+
+    return evaluated_request
+
+def update_summary(summary : Summary, request_result : dict[str,Any]) -> None:
+    """Updates the summary of the evaluated requests with the result of a single request evaluation
+
+    Args:
+        summary (Summary): The summary of the evaluated requests to update
+        request_result (dict[str,Any]): The result of the request evaluation to update the summary with
+    """
+    summary["total_requests"] += 1
+    if request_result["decision"] == "Permit":
+        summary["permitted_requests"] += 1
+    else:
+        summary["denied_requests"] += 1
+    return
+
+def test_policy(policy : dict, subject_id : str, subject : dict, resource_id : str, resource : dict, request : dict) -> tuple[bool, list[str]]:
+    """Tests if a policy satisfies a request by testing all the conditions of the policy and returns the result of the test and the matched conditions.
+
+    Args:
+        policy (dict): Policy to test
+        subject_id (str): Id of the subject
+        subject (dict): Subject data
+        resource_id (str): Resource id
+        resource (dict): Resource data
+        request (dict): Request to evaluate
+
+    Returns:
+        tuple[bool, list[str]]: Result of the evaluation and the list of the matched conditions
+    """
+    matched_conditions : list[str] = []
+    result = True
+    result, matched_subject_conditions = test_subject_conditions(policy, subject_id, subject)
+    if not result:
+        return (result, [])
+    matched_conditions.extend(matched_subject_conditions)
+    result, matched_resource_conditions = test_resource_conditions(policy, resource_id, resource)
+    if not result:
+        return (result, [])
+    matched_conditions.extend(matched_resource_conditions)
+    result, matched_action_conditions = test_action_conditions(policy, request)
+    if not result:
+        return (result, [])
+    matched_conditions.extend(matched_action_conditions)
+    result, matched_context_conditions = test_context_conditions(policy, request)
+    if not result:
+        return (result, [])
+    matched_conditions.extend(matched_context_conditions)
+    return (result, matched_conditions)
+
+def test_subject_conditions(policy : dict, subject_id : str, subject : dict) -> tuple[bool, list[str]]:
+    matched_conditions : list[str] = []
+    result = True
+    return (result, matched_conditions)
+
+def test_resource_conditions(policy : dict, resource_id : str, resource : dict) -> tuple[bool, list[str]]:
+    matched_conditions : list[str] = []
+    result = True
+    return (result, matched_conditions)
+
+def test_action_conditions(policy : dict, request : dict) -> tuple[bool, list[str]]:
+    matched_conditions : list[str] = []
+    result = True
+    return (result, matched_conditions)
+
+def test_context_conditions(policy : dict, request : dict) -> tuple[bool, list[str]]:
+    matched_conditions : list[str] = []
+    result = True
+    return (result, matched_conditions)
